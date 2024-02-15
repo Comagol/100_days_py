15ee1ae007a54fe3ff026ddb21354a676cac855e
Comagol <74417121+Comagol@users.noreply.github.com>
2024-02-15 13:19:19 +0000

    Initial commit
    First day code challenge

---
+++ main.py
@@ -0,0 +1,9 @@
+print("""
+Francisco
+Febrery 14th 2024
+I am signing up for Replit's 100 days of Python challenge!
+I will make sure to spend some time every day coding along, for a minimum of 10 minutes a day.
+I'll be using Replit, an amazing online IDE so I can do this from my phone wherever I
+I am felling 🥰​
+You can follow my progress at replit.com/@comabellafranci""")
+
